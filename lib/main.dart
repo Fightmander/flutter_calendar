7e1e9201032d942898990c098374cb7f1858e65d
fernando leon <fernando.leon@netprotect.com>
2019-10-08 22:57:04 -0500

    added images icons and bottom bar to home view

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,40 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello World'),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('My Calendar'),
+          backgroundColor: Colors.blueAccent,
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.access_alarms),
+              iconSize: 30,
+              tooltip: 'Show Time',
+              onPressed: (){
+                print("el botonsillo");
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage ('images/calendar.jpg'),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.grey,
+          shape: CircularNotchedRectangle(),
+          child: Container(height: 50.0,),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            print("el bombon se la traga");
+          },
+          tooltip: 'Increment Counter',
+          child: Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
     ),
   );
